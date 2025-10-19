6712c5a7683c56028abee2354ce1ba93a6bb4c1c
Jernej Kavka (JK) [SSW] <jernej.kavka@outlook.com>
2025-10-19 18:18:35 +1000

    Add initial implementation of SleepTools for asynchronous sleep functionality

---
+++ single-file/mcp_sleep.cs
@@ -0,0 +1,30 @@
+#!/usr/bin/env dotnet
+#:sdk Microsoft.NET.Sdk.Web
+
+#:package ModelContextProtocol@*-*
+
+using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Information);
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class SleepTools
+{
+    [McpServerTool]
+    [Description("Sleep for the given number of milliseconds, then return a JSON result.")]
+    public static async Task<string> Sleep(
+        [Description("Milliseconds to pause (e.g., 1000 = 1 second)")] int ms)
+    {
+        await Task.Delay(Math.Max(0, ms));
+        return $"{{\"slept\":{ms}}}";
+    }
+}
